087161ef1db56feed1347b79e1992c21873d8794
fanta-git <fantaji4957t@gmail.com>
2023-09-22 14:49:25 +0900

    add: JSX要素内でもダブルクォートを強制

--- src/compornents/Header.tsx
+++ src/compornents/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
     <Box as={"header"} color={"black"} bgColor={"#aaaaaa"}>
       <Container maxW="container.lg">
         <HStack h={14}>
-          <Heading as='h1' fontSize="2xl" cursor="pointer">
+          <Heading as="h1" fontSize="2xl" cursor="pointer">
             <Link as={NextLink} href="/" _hover={{ textDecoration: "none" }}>
               {SITE_NAME}
             </Link>
